abaf4629d563b87bddb438cb593a3b176906f93b
keshav1245 <tangri57@gmail.com>
2021-03-29 19:26:19 +0530

    Destructuring Objects and Params

--- 04-Intro To Javascript/17_Newer_JS_Features.js
+++ 04-Intro To Javascript/17_Newer_JS_Features.js
@@ -66,3 +66,85 @@ console.log(ele4)
 console.log(eles)
 
 
+// Object Destructuring  - more common and practical
+
+let user = {
+    email : "harvey@gmail.com",
+    pass : "mnwe evkw vkj srkjv ekjrb",
+    fname : "harvey",
+    lname : "milk",
+    born : 1930,
+    died : 1978,
+    bio : "kanvksnlkvjnskj vkjwr vkj wkvn kwv",
+    city : "San Francisco",
+    state : "California"
+
+}
+
+
+let user2 = {
+    email : "stacy@gmail.com",
+    pass : "mnwe evkw vkj srkjv ekjrb",
+    fname : "Stacy",
+    lname : "Gonslaves",
+    born : 1930,
+    city : "Tulsa",
+    state : "Oklahoma"
+}
+
+// let fname = user.fname
+// let lname = user.lname
+
+// let email = user.email is equivalent to let {email} = user
+let {email, fname, lname, pass} = user
+
+let {born:birthYear = "N/A"} = user;
+
+let {city, state, died = "N/A"} = user2
+
+
+//params destructuring - commonly used with objects
+
+function fullName(user){
+    // return `${user.fname} ${user.lname}`
+    let {fname, lname} = user
+    return `${fname} ${lname}`
+}
+
+function fullName2({fname, lname}){
+    return `${fname} ${lname}`
+}
+
+let movies = [
+    {
+        title: "mov1",
+        score : 50,
+        year : 1984
+    },
+    {
+        title: "mov2",
+        score : 60,
+        year : 1994
+    },
+    {
+        title: "mov3",
+        score : 70,
+        year : 1974
+    },
+    {
+        title: "mov4",
+        score : 80,
+        year : 2004
+    },
+    {
+        title: "mov5",
+        score : 90,
+        year : 2014
+    },
+]
+
+movies.filter((movie)=>movie.score >=70)
+
+movies.filter(({score})=>score >=70)
+
+movies.map(({title, score}) => `${title} is rated ${score}`)
