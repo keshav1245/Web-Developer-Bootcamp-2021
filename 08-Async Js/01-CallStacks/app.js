a4e536389f5524a9f0cd4de06579c83fee9b4f23
keshav1245 <tangri57@gmail.com>
2021-04-12 20:04:06 +0530

    Js Function CallStacks

---
+++ 08-Async Js/01-CallStacks/app.js
@@ -0,0 +1,12 @@
+// Call Stack is the mechanism that JS interpreter uses
+// to keep track of its place in a script that calls multiple 
+// functions.
+
+
+const multiply = (x,y) => x * y;
+
+const square = (x) => multiply(x,x)
+
+function isRightTriangle(a, b, c) {
+    return square(a) + square(b) === square(c)
+}
