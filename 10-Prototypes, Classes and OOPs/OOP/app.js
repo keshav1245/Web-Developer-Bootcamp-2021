9b4ac676a4c2eadeefcb355195a3eae330bb9375
keshav1245 <tangri57@gmail.com>
2021-08-08 18:01:30 +0530

    Classes, Inheritance and Extends

--- 10-Prototypes, Classes and OOPs/OOP/app.js
+++ 10-Prototypes, Classes and OOPs/OOP/app.js
@@ -86,3 +86,144 @@ let color2 = new Color(0,0,0)
 // Returns this if the function doesn't return an object.
 
 // Now we can add methods not to instances but prototypes
+
+
+// CLASSES -> PRETTIER SYNTAX, SAME FUNCTIONALITY
+
+class newColor {
+    constructor(r,g,b, name){//Executes immediately, when a obj is created
+        this.r = r
+        this.g = g
+        this.b = b
+        this.name = name
+        this.calcHSL()
+    }
+
+    greet(){
+        return `Hello, I am ${this.name}!`
+    }
+
+    innerRGB(){
+        const {r,g,b} = this // Destructuring
+        return `${r}, ${g}, ${b}`
+    }
+
+    rgb(){
+        return `rgb(${this.innerRGB()})`;
+    }
+
+    rgba(a=1){
+        return `rgba(${this.innerRGB()}, ${a})`
+    }
+
+    hex(){
+        const {r,g,b} = this;
+        return "#" + ((1 << 24) + (r<<16) + (g << 8) + b ).toString(16).slice(1);
+    }
+
+    fullSaturation(){
+        const {h, l} = this;
+        return `hsl(${h}, 100%, ${l}%)`;
+    }
+
+    opposite(){
+        const {h, s, l} = this;
+        return `hsl(${(h+180)%360}, ${s}%, ${l}%)`;
+    }
+
+    hsl(){
+        const {h, s, l} = this;
+        return `hsl(${h}, ${s}%, ${l}%)`;
+    }
+
+    calcHSL() {
+		let { r, g, b } = this;
+		// Make r, g, and b fractions of 1
+		r /= 255;
+		g /= 255;
+		b /= 255;
+
+		// Find greatest and smallest channel values
+		let cmin = Math.min(r, g, b),
+			cmax = Math.max(r, g, b),
+			delta = cmax - cmin,
+			h = 0,
+			s = 0,
+			l = 0;
+		if (delta == 0) h = 0;
+		else if (cmax == r)
+			// Red is max
+			h = ((g - b) / delta) % 6;
+		else if (cmax == g)
+			// Green is max
+			h = (b - r) / delta + 2;
+		else
+			// Blue is max
+			h = (r - g) / delta + 4;
+
+		h = Math.round(h * 60);
+
+		// Make negative hues positive behind 360°
+		if (h < 0) h += 360;
+		// Calculate lightness
+		l = (cmax + cmin) / 2;
+
+		// Calculate saturation
+		s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+		// Multiply l and s by 100
+		s = +(s * 100).toFixed(1);
+		l = +(l * 100).toFixed(1);
+		this.h = h;
+		this.s = s;
+		this.l = l;
+	}
+
+
+}
+let col1  = new newColor(255,40,100, 'Smooth Pink')
+let col2  = new newColor(255,255,255, 'White')
+let col3  = new newColor(200,40,100, 'Maroom')
+
+
+// Extends and Super
+
+
+class Pet{
+
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+
+    eat(){
+        return `${this.name} is eating...`
+    }
+
+}
+
+class Cat extends Pet{
+    
+    constructor(name, age, breed = 'stray'){
+        super(name, age)
+        this.breed = breed;
+    }
+
+    meow(){
+        return "MEAAOOW"
+    }
+}
+
+class Dog extends Pet{
+    
+
+    bark(){
+        return "WOOFF..."
+    }
+
+    eat(){
+        return `${this.name} scarfs his food..`
+    }
+
+}
+
