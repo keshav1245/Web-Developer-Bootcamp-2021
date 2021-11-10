1bb0075464771151394a59ec234717e5c58f5d1e
keshav1245 <tangri57@gmail.com>
2021-11-10 17:39:15 +0530

    Added Start script, added port from env

--- FINAL-PROJECT-YELPCAMP/app.js
+++ FINAL-PROJECT-YELPCAMP/app.js
@@ -189,7 +189,7 @@ app.use((err, req, res, next)=>{
     res.status(status)
     res.render('error', {err})
 })
-
-app.listen(3000, () => {
-    console.log('Serving on Localhost:3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log('Serving on Localhost:'+port)
 })
